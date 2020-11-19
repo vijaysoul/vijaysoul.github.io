bfb7323983159f116967bf49c52116ea9860b174
vijaysoul <evijay77@gmail.com>
2020-11-19 08:38:58 -0800

    profile changed

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,19 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div class="container">
+      <img alt="profile pic" src="../assets/profile.jpg" width="200px" />
+      <p style="font-size:30px; font-style:block">👋 Hi I'M VIJAY</p>
+      <p style="font-size:20px; font-style:block">💻 Software Engineer</p>
+      <br>
+      <p style="font-size:20px">📍 Tiruchirappalli, Tamil Nadu, India. </p>
+      <br>
+      <p>தெய்வத்தான் ஆகா தெனினும் முயற்சிதன்</p>
+	  <p>மெய்வருத்தக் கூலி தரும். </p>
+      <!-- <div style="display: inline-block; padding: 30px">
+      	<a style="padding: 10px">Github</a>
+      	<a style="padding: 10px">Gitlab</a>
+      	<a style="padding: 10px">Facebook</a>
+      </div> -->
+    </div>
   </div>
 </template>
-
-<script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
-export default {
-  name: "Home",
-  components: {
-    HelloWorld
-  }
-};
-</script>
